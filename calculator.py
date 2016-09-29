1c14677dcf665fa918cb0ed43e1fb8a0d7206411
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-29 12:08:37 -0700

    Removed redundant integer conversion in if statement, clarified docstring, and improved variable name for calculator_list.

--- calculator.py
+++ calculator.py
@@ -19,17 +19,19 @@ print "For each arithmetic statement, please start with the operator. Enter q at
 
 
 def intifier(tokens):
-	""" Takes a list of strings and converts all but the first to integers """
+	""" Returns a list of an operator and integers.
+
+	Takes a list of strings and converts all but the first to integers.	  """
 
 	#starts off list with operator
-	token_int = [tokens[0]]
+	calculator_list = [tokens[0]]
 
-	#change each stringy number to an integer and add it to token_int
+	#change each stringy number to an integer and add it to calculator_list
 	for each in tokens[1:]:
 		each = int(each)
-		token_int.append(each)
+		calculator_list.append(each)
 
-	return token_int
+	return calculator_list
 
 
 #create a while loop
@@ -40,23 +42,23 @@ while True:
 	tokens = intifier(tokens_input)
 
 	#Checks operator and calls arithmetic function accordingly
-	if tokens[0] == "q":
+	if tokens[0] == "q" or tokens[0] == "quit":
 		break
 	elif tokens[0] == "+":
-		print add(int(tokens[1]), int(tokens[2]))
+		print add(tokens[1], tokens[2])
 	elif tokens[0] == "-":
-		print subtract(int(tokens[1]), int(tokens[2]))
+		print subtract(tokens[1], tokens[2])
 	elif tokens[0] == "*":
-		print multiply(int(tokens[1]), int(tokens[2]))
+		print multiply(tokens[1], tokens[2])
 	elif tokens[0] == "/":
-		print divide(int(tokens[1]), int(tokens[2]))
+		print divide(tokens[1], tokens[2])
 	elif tokens[0] == "**2":
-		print square(int(tokens[1]))
+		print square(tokens[1])
 	elif tokens[0] == "**3":
-		print cube(int(tokens[1]))
+		print cube(tokens[1])
 	elif tokens[0] == "**":
-		print power(int(tokens[1]), int(tokens[2]))
+		print power(tokens[1], tokens[2])
 	elif tokens[0] == "%":
-		print mod(int(tokens[1]), int(tokens[2]))
+		print mod(tokens[1], tokens[2])
 
 
